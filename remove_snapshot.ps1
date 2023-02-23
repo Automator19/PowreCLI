e62ac37330b16088baa9d3717daf7e34c67f3ddc
RIJatinPatel <110600757+RIJatinPatel@users.noreply.github.com>
2023-02-23 16:42:02 +0000

    asdfsd

--- remove_snapshot.ps1
+++ remove_snapshot.ps1
@@ -8,6 +8,6 @@ Connect-VIServer -Server servername
 foreach ($line in $csv)
 {
   $vm=$line.vm
-  try { $out=Get-snapshot -vm $vm | Remove-Snapshot -runasync -confirm:$false }
+  try { $out = Get-snapshot -vm $vm | Remove-Snapshot -runasync -confirm:$false }
   catch { Write-Output "$vmname - $($_.Exception.Message)"}
 }
